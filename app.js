f8af986574be706a6a0eaf5ad6ca8ff3df56a427
YonugJones <peterckerfoot@gmail.com>
2024-09-25 11:43:53 -0500

    added messages to indexRouter

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const indexRouter = require('./routes/indexRouter');
+const newRouter = require('./routes/newRouter');
+const path = require("node:path");
 
-app.get('/', (req, res) => {
-  res.send('Hello world!')
-})
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', indexRouter);
+app.use('/new', newRouter);
 
 const port = process.env.PORT || 3030;
-
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
